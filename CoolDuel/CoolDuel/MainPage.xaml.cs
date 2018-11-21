6965ab1bab13f384f64733697b17dec4de1bc6c9
jakejgordon <jakejgordon@gmail.com>
2018-11-20 23:31:50 -0500

    have a simple grid with ability to set some basic info for character 1

--- CoolDuel/CoolDuel/MainPage.xaml.cs
+++ CoolDuel/CoolDuel/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using FocusState = Windows.UI.Xaml.FocusState;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,26 @@ namespace CoolDuel
         public MainPage()
         {
             this.InitializeComponent();
+            Character1Name.IsEnabled = true;
+            Character1Name.Focus(FocusState.Programmatic);
+        }
+
+        private void Character1Name_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            Character1Name.IsEnabled = false;
+        }
+
+        private void AddHitPoints_Click(object sender, RoutedEventArgs e)
+        {
+            var availablePoints = int.Parse(Character1AvailablePoints.Text);
+            var hitPoints = int.Parse(Character1HitPoints.Text);
+            Character1HitPoints.Text = (hitPoints + 5).ToString();
+            Character1AvailablePoints.Text = (availablePoints - 1).ToString();
+
+            if (availablePoints == 0)
+            {
+                Character1HitPoints.Text = (hitPoints + 5).ToString();
+            }
         }
     }
 }
