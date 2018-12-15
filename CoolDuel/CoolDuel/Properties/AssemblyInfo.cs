4f62e1b8902d28d426b4835f1c41cfb0a1226c35
jakejgordon <jakejgordon@gmail.com>
2018-12-15 09:52:50 -0500

    Preparing for store publish

--- CoolDuel/CoolDuel/Properties/AssemblyInfo.cs
+++ CoolDuel/CoolDuel/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CoolDuel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Cool Duel")]
+[assembly: AssemblyDescription("UWP 1v1 hotseat role playing game")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CoolDuel")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("jakejgordon")]
+[assembly: AssemblyProduct("Cool Duel")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
